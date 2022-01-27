a0564fdd239070c0304b385a98c893db943aec60
Jared Burkamper <jaredburkamper@gmail.com>
2022-01-27 10:56:08 -0600

    Initial commit from spring initilizr

---
+++ src/test/java/com/revature/accountmanagementbackend/AccountManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.accountmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
